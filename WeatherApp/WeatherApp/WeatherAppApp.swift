61f6f2679e974c7898445b016b5ea91442df70eb
Palvalv <101711875+Palvalv@users.noreply.github.com>
2023-11-24 14:32:13 +0100

    Second commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Pablo Álvarez Álvarez on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
